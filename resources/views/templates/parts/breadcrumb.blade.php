364da21e7513db903d8a5a6b504100875aa65cc8
Макаров Александр Михайлович <the_alex_mark@mail.ru>
2022-05-15 19:23:54 +0300

    style: Приборка в проекте

--- resources/views/templates/parts/breadcrumb.blade.php
+++ resources/views/templates/parts/breadcrumb.blade.php
@@ -1,7 +1,7 @@
-<div class="container">
-    <div class="row">
-        <div class="col-12">
+{{--<div class="container">--}}
+{{--    <div class="row">--}}
+{{--        <div class="col-12">--}}
 
-        </div>
-    </div>
-</div>
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
